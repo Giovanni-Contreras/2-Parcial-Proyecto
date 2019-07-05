b16de7df53e65086fc0fa49e48393a8dcc28850e
Giovanni Contreras <50603265+Giovanni-Contreras@users.noreply.github.com>
2019-07-04 20:48:18 -0500

    Add files via upload

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton singleton = new Singleton();
+
+            Singleton singleton = Singleton.getInstancia();
+            Singleton singleton2 = Singleton.getInstancia();
+            if (singleton==singleton2) {
+                Console.WriteLine("Es la misma instancia");
+             
+            }
+            Otra otra = new Otra();
+            Otra otra2 = new Otra();
+            if (otra == otra2)
+            {
+                Console.WriteLine("Es la misma instancia");
+            }
+            else {
+                Console.WriteLine("Es diferente instancia");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Singleton
+    {
+        private static Singleton instancia;
+        private Singleton() { }
+        public static Singleton getInstancia() {
+            if (instancia==null) {
+                instancia = new Singleton();
+            }
+            return instancia;
+        }
+    }
+}
